528e436033164fbad562b97e8093b46420afa864
Hadi Eskandari <h.eskandari@gmail.com>
2013-07-16 22:02:45 +1000

    PF-122 Correctly parsing xml content for the messages retrieved from management api

--- NServiceBus.Profiler.Core/MessageDecoders/XmlContentDecoder.cs
+++ NServiceBus.Profiler.Core/MessageDecoders/XmlContentDecoder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text;
 using System.Xml;
 
@@ -8,21 +9,52 @@ namespace NServiceBus.Profiler.Core.MessageDecoders
         public DecoderResult<XmlDocument> Decode(byte[] content)
         {
             var doc = new XmlDocument();
+            var xml = string.Empty;
 
             if (content != null && content.Length > 0)
             {
-                try
+                xml = Encoding.UTF8.GetString(content);
+                if (!TryLoadIntoDocument(xml, doc))
                 {
-                    var xml = Encoding.UTF8.GetString(content);
-                    doc.LoadXml(xml);
-                    return new DecoderResult<XmlDocument>(doc);
-                }
-                catch
-                {
+                    //TODO: Issues RESTSharp deserializer when reading byte array as string
+                    xml = GetFromBase64String(content);
+                    if(TryLoadIntoDocument(xml, doc))
+                    {
+                        return new DecoderResult<XmlDocument>(doc);
+                    }
                 }
             }
 
             return new DecoderResult<XmlDocument>(doc, false);
+        }
+
+        private static string GetFromBase64String(byte[] content)
+        {
+            try
+            {
+                var base64EncodedString = Encoding.UTF8.GetString(content);
+                var encodedDataAsBytes = Convert.FromBase64String(base64EncodedString);
+                return Encoding.UTF8.GetString(encodedDataAsBytes);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        private bool TryLoadIntoDocument(string xml, XmlDocument document)
+        {
+            if (string.IsNullOrWhiteSpace(xml)) return false;
+
+            try
+            {
+                document.LoadXml(xml);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         DecoderResult IContentDecoder.Decode(byte[] content)
