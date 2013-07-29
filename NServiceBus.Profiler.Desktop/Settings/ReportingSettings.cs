4a6710604cdf409ad0cb8ac845f20b0f775daf8b
Hadi Eskandari <h.eskandari@gmail.com>
2013-07-29 12:24:13 +1000

    Adding the missing setting file

---
+++ NServiceBus.Profiler.Desktop/Settings/ReportingSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace NServiceBus.Profiler.Desktop.Settings
+{
+    public class ReportingSettings
+    {
+        [DisplayName("Usage Data Collection Approved")]
+        [DefaultValue(false)]
+        public bool DataCollectionApproved { get; set; }
+    }
+}
