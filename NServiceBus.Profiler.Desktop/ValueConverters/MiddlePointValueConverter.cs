dd75eb180a55fa6b3b023938e1c3f7980bff2be0
Hadi Eskandari <h.eskandari@gmail.com>
2013-10-09 22:34:21 +1100

    Connection lines with middle arrow heads

---
+++ NServiceBus.Profiler.Desktop/ValueConverters/MiddlePointValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NServiceBus.Profiler.Desktop.ValueConverters
+{
+    public class MiddlePointValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (values.Length != 2)
+            {
+                throw new ArgumentException("MiddlePointValueConverter (value)");
+            }
+
+            if (values[0] == DependencyProperty.UnsetValue ||
+                values[1] == DependencyProperty.UnsetValue)
+                return 0.0;
+
+            var fromPoint = (double) values[0];
+            var toPoint = (double) values[1];
+
+            return fromPoint +
+                   (toPoint - fromPoint)/2;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ NServiceBus.Profiler.Desktop/ValueConverters/StartSegmentValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Data;
+using Mindscape.WpfDiagramming.Foundation;
+
+namespace NServiceBus.Profiler.Desktop.ValueConverters
+{
+    public class StartSegmentValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var segments = (ObservableCollection<DiagramConnectionSegment>)value;
+
+            if (parameter == null)
+            {
+                return segments.First().StartPoint;
+            }
+            else if (parameter.ToString() == "X")
+            {
+                return segments.First().StartPoint.X;
+            }
+            else
+            {
+                return segments.First().StartPoint.Y;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
