8b6cf4eebb98b5be087522c7eb09db8f2526be11
Hadi Eskandari <h.eskandari@gmail.com>
2013-10-24 13:57:04 +1100

    Fixing zoom issue with the message node popups

---
+++ NServiceBus.Profiler.Desktop/ValueConverters/ZoomToCancelScaleConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace NServiceBus.Profiler.Desktop.ValueConverters
+{
+    public class ZoomToCancelScaleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double zoom = (double)value;
+            return new ScaleTransform(1 / zoom, 1 / zoom);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
