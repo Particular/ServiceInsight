59875d29128849729856ce135d76d7822213c832
joaquinjares <joaquinjares@hotmail.com>
2013-11-13 15:19:12 -0300

    Fixed context menu, added tooltips

---
+++ NServiceBus.Profiler.Desktop/ValueConverters/HalvingConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NServiceBus.Profiler.Desktop.ValueConverters
+{
+    class HalvingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (double)value / 2.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
