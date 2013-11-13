a97ceeb80703692df0c031659368c348df92ff0b
joaquinjares <joaquinjares@hotmail.com>
2013-11-13 14:31:35 -0300

    Changed lines in arrow, moved context menu

---
+++ NServiceBus.Profiler.Desktop/ValueConverters/PercentConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NServiceBus.Profiler.Desktop.ValueConverters
+{
+    class PercentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (double)value * double.Parse((string)parameter) / 100;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NServiceBus.Profiler.Desktop/ValueConverters/SubstractionConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace NServiceBus.Profiler.Desktop.ValueConverters
+{
+    class SubstractionConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values[0] is double)
+            {
+                var ret = (double)values[0];
+                for (int i = 1; i < values.Length; i++)
+                {
+                    ret = ret - (double)values[i];
+                }
+                return ret;
+            }
+            else
+                return 0.0;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
