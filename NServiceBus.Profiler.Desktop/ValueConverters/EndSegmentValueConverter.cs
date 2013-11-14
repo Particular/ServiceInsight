d94a95140cc944eeeff97a11ff05be8182bfbf95
joaquinjares <joaquinjares@hotmail.com>
2013-11-14 10:57:01 -0300

    Fixed #62
    The arrows where centered, but on the head of the arrow instead of the
    center. Fixed now.

--- NServiceBus.Profiler.Desktop/ValueConverters/EndSegmentValueConverter.cs
+++ NServiceBus.Profiler.Desktop/ValueConverters/EndSegmentValueConverter.cs
@@ -8,9 +8,13 @@ namespace NServiceBus.Profiler.Desktop.ValueConverters
 {
     public class EndSegmentValueConverter : IValueConverter
     {
+        private const double arrowSize = 20;
+
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             var segments = (ObservableCollection<DiagramConnectionSegment>)value;
+
+            var angle = AngleFromSegments(segments);
 
             if (parameter == null)
             {
@@ -18,12 +22,17 @@ namespace NServiceBus.Profiler.Desktop.ValueConverters
             }
             if (parameter.ToString() == "X")
             {
-                return segments.Last().EndPoint.X;
+                return segments.Last().EndPoint.X + (arrowSize * Math.Sin(angle));
             }
             else
             {
-                return segments.Last().EndPoint.Y;
+                return segments.Last().EndPoint.Y + (arrowSize * Math.Cos(angle));
             }
+        }
+
+        private static double AngleFromSegments(ObservableCollection<DiagramConnectionSegment> segments)
+        {
+            return Math.Atan2(segments.Last().EndPoint.X - segments.First().EndPoint.X, segments.Last().EndPoint.Y - segments.First().EndPoint.Y);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
