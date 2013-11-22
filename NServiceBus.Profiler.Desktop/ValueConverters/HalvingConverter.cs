87bc68b39f7632293a58857eb4cc326c86557bee
joaquinjares <joaquinjares@hotmail.com>
2013-11-22 11:43:17 -0300

    Fixed #103

--- NServiceBus.Profiler.Desktop/ValueConverters/HalvingConverter.cs
+++ NServiceBus.Profiler.Desktop/ValueConverters/HalvingConverter.cs
@@ -11,7 +11,7 @@ namespace NServiceBus.Profiler.Desktop.ValueConverters
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return (double)value / 2.0;
+            return (double)value * -1.0;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
