aefb563687d6d0592b7bd28b0cae1e1904bdee35
Andreas Öhlund <andreas.ohlund@particular.net>
2013-11-29 21:15:04 +0100

    Enabling automatic versioning with gitflowversion

--- src/ServiceInsight.Desktop/Properties/AssemblyInfo.cs
+++ src/ServiceInsight.Desktop/Properties/AssemblyInfo.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Security;
 using System.Runtime.CompilerServices;
 using ExceptionHandler;
 
 [assembly: ComVisible(false)]
 [assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("")]
 [assembly: AssemblyCopyright("Copyright © 2013 Particular Software Incorporated. All rights reserved")]
 [assembly: AssemblyProduct("ServiceInsight for NServiceBus ChangeSet 21fa9a4")]
 [assembly: AssemblyCompany("Particular Software Incorporated")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyInformationalVersion("ChangeSet 21fa9a4")]
 [assembly: SupportWebUrl("http://www.particular.net")]
 [assembly: InternalsVisibleTo("Particular.ServiceInsight.Tests")]
 
