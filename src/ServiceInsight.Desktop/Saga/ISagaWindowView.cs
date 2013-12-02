59d993d457913a72ea7cec6451ba0a119bca0243
joaquinjares <joaquinjares@hotmail.com>
2013-12-02 13:59:19 -0300

    added saga

---
+++ src/ServiceInsight.Desktop/Saga/ISagaWindowView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NServiceBus.Profiler.Desktop.Saga
+{
+    public interface ISagaWindowView
+    {
+        void Initialize();
+    }
+}
---
+++ src/ServiceInsight.Desktop/Saga/SagaWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Caliburn.PresentationFramework.Screens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NServiceBus.Profiler.Desktop.Saga
+{
+    public class SagaWindowViewModel : Screen, ISagaWindowViewModel
+    {
+        private ISagaWindowView _view;
+
+        public override void AttachView(object view, object context)
+        {
+            base.AttachView(view, context);
+            _view = (ISagaWindowView)view;
+
+            this.Name = "OrderProcessed";
+        }
+
+        public string Name { get; private set; }
+
+        public Guid Guid { get; private set; }
+    }
+
+    public interface ISagaWindowViewModel : IScreen
+    {
+    }
+}
