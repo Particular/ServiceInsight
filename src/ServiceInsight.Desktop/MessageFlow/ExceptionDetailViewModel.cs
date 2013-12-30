0db1f5bc676e4093bc499829421fad0ce02f4373
joaquinjares <joaquinjares@hotmail.com>
2013-12-30 11:21:21 -0300

    Fixes #139

--- src/ServiceInsight.Desktop/MessageFlow/ExceptionDetailViewModel.cs
+++ src/ServiceInsight.Desktop/MessageFlow/ExceptionDetailViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Caliburn.PresentationFramework.Screens;
+using NServiceBus.Profiler.Desktop.Core.Settings;
 using NServiceBus.Profiler.Desktop.Models;
+using NServiceBus.Profiler.Desktop.Shell;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +11,47 @@ namespace NServiceBus.Profiler.Desktop.MessageFlow
 {
     class ExceptionDetailViewModel : Screen, IExceptionDetailViewModel
     {
+        private ISettingsProvider _settingsProvider;
+        public virtual IPersistableLayout View { get; private set; }
+
         public IExceptionDetails Exception { get; set; }
 
-        public ExceptionDetailViewModel() { }
+        public ExceptionDetailViewModel(ISettingsProvider settingsProvider) 
+        {
+            this._settingsProvider = settingsProvider;
+        }
 
         public ExceptionDetailViewModel(IExceptionDetails exception)
         {
             this.Exception = exception;
+        }
+
+        public override void AttachView(object view, object context)
+        {
+            base.AttachView(view, context);
+            View = (IPersistableLayout)view;
+        }
+
+        protected override void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            RestoreLayout();
+        }
+
+        public virtual void Deactivate(bool close)
+        {
+            base.OnDeactivate(close);
+            SaveLayout();
+        }
+
+        private void SaveLayout()
+        {
+            View.OnSaveLayout(_settingsProvider);
+        }
+
+        private void RestoreLayout()
+        {
+            View.OnRestoreLayout(_settingsProvider);
         }
 
         public string FormattedSource
