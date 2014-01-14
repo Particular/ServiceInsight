49bfdb97ad73959de9cde74acdd2f438241ce547
joaquinjares <joaquinjares@hotmail.com>
2014-01-14 15:29:12 -0300

    Added proper formatting for Saga Header
    Kind of a lot of duplicated code. Solve that.

--- src/ServiceInsight.Desktop/Saga/SagaData.cs
+++ src/ServiceInsight.Desktop/Saga/SagaData.cs
@@ -9,7 +9,34 @@ namespace NServiceBus.Profiler.Desktop.Saga
     public class SagaData
     {
         public List<SagaUpdate> Changes { get; set; }
-        public string SagaType { get; set; }
+
+        private string sagaType;
+        public string SagaType
+        { 
+            get 
+            {
+                return ProcessType(sagaType);
+            } 
+            set 
+            {
+                sagaType = value;
+            } 
+        }
+
+        private string ProcessType(string messageType)
+        {
+            if (string.IsNullOrEmpty(messageType))
+                return string.Empty;
+
+            var clazz = messageType.Split(',').First();
+            var objectName = clazz.Split('.').Last();
+
+            if (objectName.Contains("+"))
+                objectName = objectName.Split('+').Last();
+
+            return objectName;
+        }
+
         public Guid SagaId { get; set; }
 
         public bool IsCompleted
