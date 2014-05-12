6708931b5463d95ac7a1cee31c0284f340bac99d
Simon Cropp <simon.cropp@gmail.com>
2014-05-12 15:38:35 +1000

    remove redundant method

--- src/ServiceInsight.Desktop/MessageViewers/MessageBodyView.xaml.cs
+++ src/ServiceInsight.Desktop/MessageViewers/MessageBodyView.xaml.cs
@@ -1,22 +1,10 @@
 ﻿namespace Particular.ServiceInsight.Desktop.MessageViewers
 {
-    using System.Windows;
-    using DevExpress.Xpf.Core;
-
-    /// <summary>
-    /// Interaction logic for MessageBodyView.xaml
-    /// </summary>
     public partial class MessageBodyView
     {
         public MessageBodyView()
         {
             InitializeComponent();
-        }
-
-        void OnSelectedTabChanged(object sender, ValueChangedEventArgs<FrameworkElement> e)
-        {
-            var newElement = e.NewValue;
-            var oldElement = e.OldValue;
         }
     }
 }
