94339601d2924aea190484f21922712651370dd0
Simon Cropp <simon.cropp@gmail.com>
2014-05-12 14:48:58 +1000

    remove un used methods

--- src/ServiceInsight.Desktop/ExtensionMethods/CollectionExtensions.cs
+++ src/ServiceInsight.Desktop/ExtensionMethods/CollectionExtensions.cs
@@ -13,17 +13,5 @@
             }
         }
 
-        public static bool IsEmpty<T>(this IList<T> list)
-        {
-            return list == null || list.Count == 0;
-        }
-
-        public static void AddRange<T>(this IList<T> list, IList<T> collectionToAdd)
-        {
-            foreach (var item in collectionToAdd)
-            {
-                list.Add(item);
-            }
-        }
     }
 }
