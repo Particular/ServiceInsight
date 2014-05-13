06f2e7382048991a90d0175871209aa66cff9058
Simon Cropp <simon.cropp@gmail.com>
2014-05-13 15:32:03 +1000

    remove un-used guard methods

--- src/ServiceInsight.Desktop/Guard.cs
+++ src/ServiceInsight.Desktop/Guard.cs
@@ -21,30 +21,6 @@
             }
         }
 
-        public static void True(bool expression)
-        {
-            if (!expression)
-                throw new InvalidOperationException("Value was not expected");
-        }
-
-        public static void False(bool expression)
-        {
-            if (expression)
-                throw new InvalidOperationException("Value was not expected");
-        }
-
-        public static void True(bool expression, Func<Exception> error)
-        {
-            if (!expression)
-                throw error();
-        }
-
-        public static void False(bool expression, Func<Exception> error)
-        {
-            if (expression)
-                throw error();
-        }
-
         /// <summary>
         /// Ensures the given string <paramref name="value"/> is not null or empty.
         /// Throws <see cref="ArgumentNullException"/> in the first case, or 
@@ -57,35 +33,6 @@
                 throw new ArgumentException(errorMessage, GetParameterName(reference));
         }
 
-        /// <summary>
-        /// Checks an argument to ensure it is in the specified range including the edges.
-        /// </summary>
-        /// <typeparam name="T">Type of the argument to check, it must be an <see cref="IComparable"/> type.
-        /// </typeparam>
-        /// <param name="reference">The expression containing the name of the argument.</param>
-        /// <param name="value">The argument value to check.</param>
-        /// <param name="from">The minimun allowed value for the argument.</param>
-        /// <param name="to">The maximun allowed value for the argument.</param>
-        public static void NotOutOfRangeInclusive<T>(Expression<Func<T>> reference, T value, T from, T to) where T : IComparable
-        {
-            if ((value.CompareTo(from) < 0 || value.CompareTo(to) > 0))
-                throw new ArgumentOutOfRangeException(GetParameterName(reference));
-        }
-
-        /// <summary>
-        /// Checks an argument to ensure it is in the specified range excluding the edges.
-        /// </summary>
-        /// <typeparam name="T">Type of the argument to check, it must be an <see cref="IComparable"/> type.
-        /// </typeparam>
-        /// <param name="reference">The expression containing the name of the argument.</param>
-        /// <param name="value">The argument value to check.</param>
-        /// <param name="from">The minimun allowed value for the argument.</param>
-        /// <param name="to">The maximun allowed value for the argument.</param>
-        public static void NotOutOfRangeExclusive<T>(Expression<Func<T>> reference, T value, T from, T to) where T : IComparable
-        {
-            if ((value.CompareTo(from) <= 0 || value.CompareTo(to) >= 0))
-                throw new ArgumentOutOfRangeException(GetParameterName(reference));
-        }
 
         static string GetParameterName(LambdaExpression reference)
         {
