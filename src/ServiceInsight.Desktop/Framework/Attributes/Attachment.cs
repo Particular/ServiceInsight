8d114916d70f167763bed0ca95115f693095af76
Cameron MacFarland <distantcam@gmail.com>
2014-05-17 10:33:26 +0800

    Added attachments

---
+++ src/ServiceInsight.Desktop/Framework/Attributes/Attachment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Particular.ServiceInsight.Desktop.Framework.Attributes
+{
+    internal interface IAttachment
+    {
+        void AttachTo(object obj);
+    }
+
+    public abstract class Attachment<T> : IAttachment
+    {
+        protected T instance;
+
+        protected abstract void OnAttach();
+
+        void IAttachment.AttachTo(object obj)
+        {
+            instance = (T)obj;
+            OnAttach();
+        }
+    }
+}
---
+++ src/ServiceInsight.Desktop/Framework/Attributes/AutoAttachmentModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Autofac;
+using Autofac.Core;
+
+namespace Particular.ServiceInsight.Desktop.Framework.Attributes
+{
+    class AutoAttachmentModule : Module
+    {
+        protected override void AttachToComponentRegistration(
+            IComponentRegistry componentRegistry,
+            IComponentRegistration registration)
+        {
+            registration.Activating += Activating;
+        }
+
+        private void Activating(object sender, ActivatingEventArgs<object> e)
+        {
+            var vmType = e.Instance.GetType();
+
+            if (!vmType.FullName.EndsWith("ViewModel"))
+                return;
+
+            // Convention to find attachments from a ViewModel
+            // This can be done better.
+            var attachmentType = Type.GetType(vmType.FullName.Replace("ViewModel", "Attachment"));
+
+            if (attachmentType == null || !e.Context.IsRegistered(attachmentType))
+                return;
+
+            var attachment = (IAttachment)e.Context.Resolve(attachmentType);
+
+            attachment.AttachTo(e.Instance);
+        }
+    }
+}
