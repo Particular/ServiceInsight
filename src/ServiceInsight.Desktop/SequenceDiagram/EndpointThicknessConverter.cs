d0dd7cb974b858a1096bca41f4c8e68e5964807a
Cameron MacFarland <distantcam@gmail.com>
2014-07-04 12:17:16 +0800

    Added start message arrow

--- src/ServiceInsight.Desktop/SequenceDiagram/EndpointThicknessConverter.cs
+++ src/ServiceInsight.Desktop/SequenceDiagram/EndpointThicknessConverter.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Globalization;
-
 namespace Particular.ServiceInsight.Desktop.SequenceDiagram
 {
+    using System;
+    using System.Globalization;
     using System.Windows;
     using System.Windows.Data;
 
@@ -10,11 +9,15 @@ namespace Particular.ServiceInsight.Desktop.SequenceDiagram
     {
         public double ColumnWidth { get; set; }
 
+        public bool Middle { get; set; }
+
+        public double Top { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var column = (int)value;
 
-            return new Thickness(column * ColumnWidth, 0, 0, 0);
+            return new Thickness(column * ColumnWidth + (Middle ? (ColumnWidth / 2) : 0), Top, 0, 0);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
