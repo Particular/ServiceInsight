ca16ad2eeee788681b5f2e44f6e0eaf95c8c26ab
Cameron MacFarland <distantcam@gmail.com>
2014-09-05 13:30:17 +0800

    Simplified color change logic, sped up debugging

--- src/ServiceInsight.Desktop/SequenceDiagram/SequenceDiagramMessage.xaml.cs
+++ src/ServiceInsight.Desktop/SequenceDiagram/SequenceDiagramMessage.xaml.cs
@@ -1,9 +1,64 @@
 ﻿namespace Particular.ServiceInsight.Desktop.SequenceDiagram
 {
     using System.Windows;
+    using System.Windows.Media;
 
     public partial class SequenceDiagramMessage
     {
+        public static readonly DependencyProperty LineProperty = DependencyProperty.Register(
+            "Line", typeof(Brush), typeof(SequenceDiagramMessage), new PropertyMetadata(default(Brush)));
+
+        public Brush Line
+        {
+            get { return (Brush)GetValue(LineProperty); }
+            set { SetValue(LineProperty, value); }
+        }
+
+        public static readonly DependencyProperty EndpointBoxProperty = DependencyProperty.Register(
+            "EndpointBox", typeof(Brush), typeof(SequenceDiagramMessage), new PropertyMetadata(default(Brush)));
+
+        public Brush EndpointBox
+        {
+            get { return (Brush)GetValue(EndpointBoxProperty); }
+            set { SetValue(EndpointBoxProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageBackgroundProperty = DependencyProperty.Register(
+            "MessageBackground", typeof(Brush), typeof(SequenceDiagramMessage), new PropertyMetadata(default(Brush)));
+
+        public Brush MessageBackground
+        {
+            get { return (Brush)GetValue(MessageBackgroundProperty); }
+            set { SetValue(MessageBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageForegroundProperty = DependencyProperty.Register(
+            "MessageForeground", typeof(Brush), typeof(SequenceDiagramMessage), new PropertyMetadata(default(Brush)));
+
+        public Brush MessageForeground
+        {
+            get { return (Brush)GetValue(MessageForegroundProperty); }
+            set { SetValue(MessageForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty SagaBackgroundProperty = DependencyProperty.Register(
+            "SagaBackground", typeof(Brush), typeof(SequenceDiagramMessage), new PropertyMetadata(default(Brush)));
+
+        public Brush SagaBackground
+        {
+            get { return (Brush)GetValue(SagaBackgroundProperty); }
+            set { SetValue(SagaBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty SagaForegroundProperty = DependencyProperty.Register(
+            "SagaForeground", typeof(Brush), typeof(SequenceDiagramMessage), new PropertyMetadata(default(Brush)));
+
+        public Brush SagaForeground
+        {
+            get { return (Brush)GetValue(SagaForegroundProperty); }
+            set { SetValue(SagaForegroundProperty, value); }
+        }
+
         public SequenceDiagramMessage()
         {
             InitializeComponent();
