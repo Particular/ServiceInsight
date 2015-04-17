5a456af21d4c7e4cd01f6baba26b14b1ffe7a295
Cameron MacFarland <distantcam@gmail.com>
2015-04-17 13:50:37 +0800

    Cleaned up chocolatey scripts

--- src/Setup/Chocolatey/chocolateyInstall.ps1
+++ src/Setup/Chocolatey/chocolateyInstall.ps1
@@ -1,34 +1,5 @@
-﻿$packageName = "ServiceInsight"
+﻿$productName = "ServiceInsight";
 
-$url = gci -path "c:\ChocolateyResourceCache" -Filter "Particular.$packageName-*.exe" -ErrorAction SilentlyContinue| select -first 1
+$url = "https://github.com/Particular/$productName/releases/download/$env:chocolateyPackageVersion/Particular.$productName-$env:chocolateyPackageVersion.exe"
 
-if($url){
-	$url = $url | Select -expandProperty FullName
-}
-else{
-	$url = "https://github.com/Particular/$packageName/releases/download/{{ReleaseName}}/Particular.$packageName-{{FileVersion}}.exe"
-}
-
-
-try {
-
-    $chocTempDir = Join-Path $env:TEMP "chocolatey"
-    $tempDir = Join-Path $chocTempDir "$packageName"
-    if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null}
-    $file = Join-Path $tempDir "$($packageName)Install.exe"
-    $logFile = Join-Path $tempDir "msiexe.log"
-
-	If (Test-Path $logFile){
-		Remove-Item $logFile
-	}
-
-    Get-ChocolateyWebFile $packageName $file $url 
-	$msiArguments  ="/quiet  /L*V `"$logFile`""
-	Write-Host "Starting installer with arguments: $msiArguments";
-    Start-ChocolateyProcessAsAdmin "$msiArguments" $file -validExitCodes 0
-    Write-ChocolateySuccess $packageName
-    Remove-Item $file -ErrorAction SilentlyContinue 
-} catch {
-	Write-ChocolateyFailure $packageName $($_.Exception.Message)
-	throw
-}
+Install-ChocolateyPackage $productName 'exe' '/quiet' $url
