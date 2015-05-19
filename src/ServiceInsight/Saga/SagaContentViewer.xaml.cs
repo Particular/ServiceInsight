69264bdfd0810fac4c5f8121bb9399f064475d3f
John Simons <john.simons@nservicebus.com>
2015-05-19 12:53:22 +1000

    Added dark theme for xml and json highlighting

--- src/ServiceInsight/Saga/SagaContentViewer.xaml.cs
+++ src/ServiceInsight/Saga/SagaContentViewer.xaml.cs
@@ -1,8 +1,11 @@
 ﻿namespace Particular.ServiceInsight.Desktop.Saga
 {
+    using System.Collections.Generic;
     using System.Windows;
     using System.Windows.Media;
+    using System.Xml;
     using ICSharpCode.AvalonEdit.Folding;
+    using ICSharpCode.AvalonEdit.Highlighting;
     using Particular.ServiceInsight.Desktop.MessageViewers.JsonViewer;
 
     public partial class SagaContentViewer
@@ -11,13 +14,38 @@
 
         public SagaContentViewer()
         {
+            RegisterDarkThemeHighlighters();
+
             InitializeComponent();
 
             SetValue(TextOptions.TextFormattingModeProperty, TextFormattingMode.Display);
             foldingManager = FoldingManager.Install(document.TextArea);
             document.TextArea.IndentationStrategy = new ICSharpCode.AvalonEdit.Indentation.DefaultIndentationStrategy();
+            document.Options.EnableHyperlinks = false;
+            document.Options.EnableEmailHyperlinks = false;
 
             Loaded += OnLoaded;
+        }
+
+        static void RegisterDarkThemeHighlighters()
+        {
+            var highlighters = new List<string[]> { new[] { "JavaScript-Mode.xshd", "JavaScript", ".js" }, new[] { "XML-Mode.xshd", "XML", "xml" } };
+
+            foreach (var highlighter in highlighters)
+            {
+                using (var stream = typeof(SagaContentViewer).Assembly.GetManifestResourceStream("ServiceInsight.Saga.Resources." + highlighter[0]))
+                {
+                    using (XmlReader reader = new XmlTextReader(stream))
+                    {
+                        var customHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.HighlightingLoader.Load(reader, HighlightingManager.Instance);
+
+                        HighlightingManager.Instance.RegisterHighlighting(highlighter[1], new[]
+                        {
+                            highlighter[2]
+                        }, customHighlighting);
+                    }
+                }
+            }
         }
 
         void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
