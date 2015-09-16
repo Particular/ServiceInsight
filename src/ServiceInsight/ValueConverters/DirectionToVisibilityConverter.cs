2886839f66998005e38d0cb82b71adbea3e3b0fd
John Simons <john.simons@nservicebus.com>
2015-09-16 16:50:30 +1000

    Adding arrow tips

---
+++ src/ServiceInsight/ValueConverters/DirectionToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace Particular.ServiceInsight.Desktop.ValueConverters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+    using global::ServiceInsight.SequenceDiagram.Diagram;
+
+    public class DirectionToVisibilityConverter : IValueConverter
+    {
+        public Direction Direction { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Direction direction;
+            Enum.TryParse(value.ToString(), out direction);
+            return direction == Direction ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
