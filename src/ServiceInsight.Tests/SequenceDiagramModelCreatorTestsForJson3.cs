b223d27d08f8ae85792718816fa08d5dd264882c
John Simons <john.simons@nservicebus.com>
2015-09-16 14:45:35 +1000

    Added missing asserts

--- src/ServiceInsight.Tests/SequenceDiagramModelCreatorTestsForJson3.cs
+++ src/ServiceInsight.Tests/SequenceDiagramModelCreatorTestsForJson3.cs
@@ -17,6 +17,11 @@ namespace Particular.ServiceInsight.Tests
             Assert.AreEqual(7, result.Count);
             Assert.AreEqual("System.Web", result[0].Name);
             Assert.AreEqual("Particular.ApplicationProcessor.LoanProcessingSagaHandler", result[1].Name);
+            Assert.AreEqual("Particular.AnalyticsDatabase.AnalyticsDataHandler", result[2].Name);
+            Assert.AreEqual("Particular.ApplicationProcessor.LoanDecisionHandler", result[3].Name);
+            Assert.AreEqual("Particular.ApplicationProcessor.PartnerDecisionPostbackHandler", result[4].Name);
+            Assert.AreEqual("Particular.ApplicationProcessor.WebLoanDecisionUpdateHandler", result[5].Name);
+            Assert.AreEqual("Particular.ApplicationProcessor.EmailHandler", result[6].Name);
         }
     }
 }
