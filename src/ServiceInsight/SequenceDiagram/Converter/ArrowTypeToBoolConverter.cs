3255c2cb2121dc2edd71a23d6f3f6d918deb8595
John Simons <john.simons@nservicebus.com>
2015-09-22 16:35:28 +1000

    Remove ellipse for timeouts too

--- src/ServiceInsight/SequenceDiagram/Converter/ArrowTypeToBoolConverter.cs
+++ src/ServiceInsight/SequenceDiagram/Converter/ArrowTypeToBoolConverter.cs
@@ -8,13 +8,11 @@ namespace ServiceInsight.SequenceDiagram.Converter
 
     public class ArrowTypeToVisibilityConverter : IValueConverter
     {
-        public ArrowType Type { get; set; }
-
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             ArrowType arrowType;
             Enum.TryParse(value.ToString(), out arrowType);
-            return arrowType == Type ? Visibility.Collapsed : Visibility.Visible;
+            return (arrowType == ArrowType.Local || arrowType == ArrowType.Timeout) ? Visibility.Collapsed : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
