ece8df724f552975af380faea4d4484818c5fea1
Hadi Eskandari <h.eskandari@gmail.com>
2015-10-21 13:02:53 +1100

    Converting the line to Path+Segment

---
+++ src/ServiceInsight/SequenceDiagram/Converter/HeightToLineSegmentConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace ServiceInsight.SequenceDiagram.Converter
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class HeightToLinePoinConverter : IValueConverter
+    {
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double height;
+            double.TryParse(value.ToString(), out height);
+
+            return new Point(0, height);
+        }
+    }
+}
