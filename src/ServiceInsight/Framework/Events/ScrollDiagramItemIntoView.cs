c922aa07305829231295974de4f27606ef513bfb
Hadi Eskandari <h.eskandari@gmail.com>
2015-12-14 17:02:14 +1100

    Changing the tooltip header for handlers. Closes #528.

---
+++ src/ServiceInsight/Framework/Events/ScrollDiagramItemIntoView.cs
@@ -0,0 +1,14 @@
+﻿namespace Particular.ServiceInsight.Desktop.Framework.Events
+{
+    using Particular.ServiceInsight.Desktop.Models;
+
+    public class ScrollDiagramItemIntoView
+    {
+        public ScrollDiagramItemIntoView(StoredMessage message)
+        {
+            Message = message;
+        }
+
+        public StoredMessage Message { get; }
+    }
+}
--- src/ServiceInsight/Framework/Events/SelectedMessageChanged.cs
+++ src/ServiceInsight/Framework/Events/SelectedMessageChanged.cs
@@ -1,18 +1,6 @@
 ﻿namespace Particular.ServiceInsight.Desktop.Framework.Events
 {
-    using Particular.ServiceInsight.Desktop.Models;
-
     public class SelectedMessageChanged
     {
-    }
-
-    public class ScrollDiagramItemIntoView
-    {
-        public ScrollDiagramItemIntoView(StoredMessage message)
-        {
-            Message = message;
-        }
-
-        public StoredMessage Message { get; }
     }
 }
