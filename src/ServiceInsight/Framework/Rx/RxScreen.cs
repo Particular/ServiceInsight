ff8a029f603223fa42b2e732bd3eadcb3dfe02d1
Cameron MacFarland <distantcam@gmail.com>
2016-01-04 09:19:40 +0800

    Using external package to avoid copying Caliburn.Micro code.

--- src/ServiceInsight/Framework/Rx/RxScreen.cs
+++ src/ServiceInsight/Framework/Rx/RxScreen.cs
@@ -1,176 +1,26 @@
-﻿namespace ServiceInsight.Framework.Rx
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Caliburn.Micro;
+using ObservablePropertyChanged;
+
+namespace ServiceInsight.Framework.Rx
 {
-    using System;
-    using Caliburn.Micro;
-
-    public class RxScreen : RxViewAware, IScreen, IChild
+    public class RxScreen : Screen, IObservablePropertyChanged
     {
-        static readonly ILog Log = LogManager.GetLog(typeof(Screen));
+        ObservablePropertyChangeHelper helper = new ObservablePropertyChangeHelper();
 
-        bool isActive;
-        bool isInitialized;
-        object parent;
-        string displayName;
+        public IObservable<PropertyChangeData> Changed => helper.ChangedObservable;
 
-        /// <summary>
-        /// Creates an instance of the screen.
-        /// </summary>
-        public RxScreen()
+        public void Dispose()
         {
-            displayName = GetType().FullName;
+            Interlocked.Exchange(ref helper, null)?.Dispose();
         }
 
-        /// <summary>
-        /// Gets or Sets the Parent <see cref = "IConductor" />
-        /// </summary>
-        public virtual object Parent
+        public override void NotifyOfPropertyChange([CallerMemberName] string propertyName = null)
         {
-            get { return parent; }
-            set
-            {
-                parent = value;
-                NotifyOfPropertyChange("Parent");
-            }
-        }
-
-        /// <summary>
-        /// Gets or Sets the Display Name
-        /// </summary>
-        public virtual string DisplayName
-        {
-            get { return displayName; }
-            set
-            {
-                displayName = value;
-                NotifyOfPropertyChange("DisplayName");
-            }
-        }
-
-        /// <summary>
-        /// Indicates whether or not this instance is currently active.
-        /// </summary>
-        public bool IsActive
-        {
-            get { return isActive; }
-            private set
-            {
-                isActive = value;
-                NotifyOfPropertyChange("IsActive");
-            }
-        }
-
-        /// <summary>
-        /// Indicates whether or not this instance is currently initialized.
-        /// </summary>
-        public bool IsInitialized
-        {
-            get { return isInitialized; }
-            private set
-            {
-                isInitialized = value;
-                NotifyOfPropertyChange("IsInitialized");
-            }
-        }
-
-        /// <summary>
-        /// Raised after activation occurs.
-        /// </summary>
-        public event EventHandler<ActivationEventArgs> Activated = delegate { };
-
-        /// <summary>
-        /// Raised before deactivation.
-        /// </summary>
-        public event EventHandler<DeactivationEventArgs> AttemptingDeactivation = delegate { };
-
-        /// <summary>
-        /// Raised after deactivation.
-        /// </summary>
-        public event EventHandler<DeactivationEventArgs> Deactivated = delegate { };
-
-        void IActivate.Activate()
-        {
-            if (IsActive)
-            {
-                return;
-            }
-
-            var initialized = false;
-
-            if (!IsInitialized)
-            {
-                IsInitialized = initialized = true;
-                OnInitialize();
-            }
-
-            IsActive = true;
-            Log.Info("Activating {0}.", this);
-            OnActivate();
-
-            Activated(this, new ActivationEventArgs
-            {
-                WasInitialized = initialized
-            });
-        }
-
-        /// <summary>
-        /// Called when initializing.
-        /// </summary>
-        protected virtual void OnInitialize() { }
-
-        /// <summary>
-        /// Called when activating.
-        /// </summary>
-        protected virtual void OnActivate() { }
-
-        void IDeactivate.Deactivate(bool close)
-        {
-            if (IsActive || (IsInitialized && close))
-            {
-                AttemptingDeactivation(this, new DeactivationEventArgs
-                {
-                    WasClosed = close
-                });
-
-                IsActive = false;
-                Log.Info("Deactivating {0}.", this);
-                OnDeactivate(close);
-
-                Deactivated(this, new DeactivationEventArgs
-                {
-                    WasClosed = close
-                });
-
-                if (close)
-                {
-                    Views.Clear();
-                    Log.Info("Closed {0}.", this);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Called when deactivating.
-        /// </summary>
-        /// <param name = "close">Inidicates whether this instance will be closed.</param>
-        protected virtual void OnDeactivate(bool close) { }
-
-        /// <summary>
-        /// Called to check whether or not this instance can close.
-        /// </summary>
-        /// <param name = "callback">The implementor calls this action with the result of the close check.</param>
-        public virtual void CanClose(Action<bool> callback)
-        {
-            callback(true);
-        }
-
-        /// <summary>
-        /// Tries to close this instance by asking its Parent to initiate shutdown or by asking its corresponding view to close.
-        /// Also provides an opportunity to pass a dialog result to it's corresponding view.
-        /// </summary>
-        /// <param name="dialogResult">The dialog result.</param>
-        public virtual void TryClose(bool? dialogResult = null)
-        {
-            PlatformProvider.Current.GetViewCloseAction(this, Views.Values, dialogResult).OnUIThread();
+            base.NotifyOfPropertyChange(propertyName);
+            helper.PropertyChanged(this, propertyName);
         }
     }
 }
