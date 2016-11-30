e36e46e8b2b350595e2fc5730d9dc8c5bf8f3db6
Cameron MacFarland <distantcam@gmail.com>
2016-12-01 01:46:49 +0800

    Created xaml icon tests

---
+++ src/ServiceInsight.Tests/AutoApprover.cs
@@ -0,0 +1,41 @@
+﻿namespace ServiceInsight.Tests
+{
+    using System.IO;
+    using ApprovalTests.Core;
+
+    class AutoApprover : IReporterWithApprovalPower
+    {
+        public static readonly AutoApprover INSTANCE = new AutoApprover();
+
+        private string approved;
+        private string received;
+
+        public void Report(string approved, string received)
+        {
+            this.approved = approved;
+            this.received = received;
+        }
+
+        public bool ApprovedWhenReported()
+        {
+            if (!File.Exists(received))
+            {
+                return false;
+            }
+
+            File.Delete(approved);
+            if (File.Exists(approved))
+            {
+                return false;
+            }
+
+            File.Copy(received, approved);
+            if (!File.Exists(approved))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
