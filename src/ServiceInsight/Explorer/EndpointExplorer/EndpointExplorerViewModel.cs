14c7964f6401741ac5dcfd625e7443254aed8132
Hadi Eskandari <h.eskandari@gmail.com>
2017-10-09 10:35:51 +1100

    Validating existence of SC root node before switching the selected endpoint

--- src/ServiceInsight/Explorer/EndpointExplorer/EndpointExplorerViewModel.cs
+++ src/ServiceInsight/Explorer/EndpointExplorer/EndpointExplorerViewModel.cs
@@ -199,7 +199,7 @@
 
         public void Handle(RequestSelectingEndpoint message)
         {
-            if (ServiceControlRoot.EndpointExists(message.Endpoint))
+            if (ServiceControlRoot?.EndpointExists(message.Endpoint) == true)
             {
                 var node = ServiceControlRoot.GetEndpointNode(message.Endpoint);
                 SelectedNode = node;
