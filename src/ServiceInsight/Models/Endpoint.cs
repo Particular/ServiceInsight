d08da05f8bfcd476db8d6f49399456e29d7ddb04
danielmarbach <daniel.marbach@openplace.net>
2018-01-24 13:46:44 +0100

    Removed unneeded endpoint properties

--- src/ServiceInsight/Models/Endpoint.cs
+++ src/ServiceInsight/Models/Endpoint.cs
@@ -1,9 +1,5 @@
 ﻿namespace ServiceInsight.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-
     public class Endpoint
     {
         string host;
@@ -19,27 +15,6 @@
         public string HostId { get; set; }
 
         public string HostDisplayName { get; set; }
-
-        public List<EndpointProperty> EndpointProperties { get; set; }
-
-        public bool EmitsHeartbeats
-        {
-            get
-            {
-                if (EndpointProperties == null)
-                {
-                    return false;
-                }
-
-                var heartbeat = EndpointProperties.FirstOrDefault(p => string.Equals("monitored", p.Key, StringComparison.InvariantCultureIgnoreCase));
-                if (heartbeat == null)
-                {
-                    return false;
-                }
-
-                return bool.Parse(heartbeat.Value);
-            }
-        }
 
         public string Address => string.Format("{0}{1}", Name, AtMachine());
 
@@ -74,13 +49,5 @@
         public static bool operator !=(Endpoint left, Endpoint right) => !Equals(left, right);
 
         public override string ToString() => Address;
-    }
-
-    [Serializable]
-    public class EndpointProperty //not using KeyValuePair<,> as it is read-only and the rest client can't hydrate it
-    {
-        public string Key { get; set; }
-
-        public string Value { get; set; }
     }
 }
