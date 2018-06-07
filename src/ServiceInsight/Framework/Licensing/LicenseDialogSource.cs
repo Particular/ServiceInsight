2861a415a1e99c35a749977cbcd58395a43f3f64
Hadi Eskandari <h.eskandari@gmail.com>
2018-06-07 16:54:34 +1000

    Showing all validation errors when importing a license

---
+++ src/ServiceInsight/Framework/Licensing/LicenseDialogSource.cs
@@ -0,0 +1,26 @@
+﻿namespace ServiceInsight.Framework.Licensing
+{
+    using Particular.Licensing;
+
+    internal class LicenseDialogSource : LicenseSource
+    {
+        private string licenseText;
+
+        private LicenseDialogSource(string licenseText)
+            : base("license dialog")
+        {
+            this.licenseText = licenseText;
+        }
+
+        public static LicenseSourceResult Validate(string licenseText)
+        {
+            var source = new LicenseDialogSource(licenseText);
+            return source.Find("ServiceInsight");
+        }
+
+        public override LicenseSourceResult Find(string applicationName)
+        {
+            return ValidateLicense(licenseText, applicationName);
+        }
+    }
+}
