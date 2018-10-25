5188d3faf90a5da1f5212ba215a263bddd4554e2
Hadi Eskandari <h.eskandari@gmail.com>
2018-10-25 14:17:43 +1100

    Reusing the status button in app.xaml

--- src/ServiceInsight/ValueConverters/DateStatusToBrushConverter.cs
+++ src/ServiceInsight/ValueConverters/DateStatusToBrushConverter.cs
@@ -5,14 +5,15 @@
     using System.Windows;
     using System.Windows.Data;
     using System.Windows.Media;
+    using ServiceInsight.ExtensionMethods;
     using ServiceInsight.Framework.Licensing;
 
     public class DateStatusToBrushConverter : IValueConverter
     {
-        private static SolidColorBrush red = (SolidColorBrush)new BrushConverter().ConvertFrom("#E52620");
-
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            var app = Application.Current;
+
             if (value is DateExpirationStatus)
             {
                 var status = (DateExpirationStatus)value;
@@ -20,7 +21,7 @@
                     status == DateExpirationStatus.Expiring ||
                     status == DateExpirationStatus.ExpiringToday)
                 {
-                    return red;
+                    return app?.TryFindResource("ErrorStatusBrush");
                 }
             }
 
