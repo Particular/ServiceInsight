61bac76e897dd3c4a5502c65ce2f8ebf90f3727b
aleksandr-samila <aleks.samila@particular.net>
2019-11-28 23:38:02 +0200

    Cleaned up test

--- src/ServiceInsight.Tests/XmlDecoderTests.cs
+++ src/ServiceInsight.Tests/XmlDecoderTests.cs
@@ -3,7 +3,6 @@
     using System.Text;
     using System.Xml;
     using NUnit.Framework;
-    using ServiceInsight.Framework;
     using ServiceInsight.Framework.MessageDecoders;
     using Shouldly;
 
@@ -30,15 +29,6 @@
         public void Should_not_get_null_document_when_decoding_flat_strings()
         {
             decoder.Decode(new byte[0]).ShouldNotBe(null);
-        }       
-        
-        [Test]
-        public void TestUpdater()
-        {
-            var u = new UpdateStatusCheck();
-
-            //var res = u.CheckTheLatestVersion().ConfigureAwait(false);
-            var res = u.CheckTheLatestVersion();
         }
 
         [Test]
