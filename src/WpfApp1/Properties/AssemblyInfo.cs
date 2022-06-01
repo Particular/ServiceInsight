4b2b5c27aaad4179bc4bea7dcb07fe9a25ae3976
Ramon Smits <ramon.smits@gmail.com>
2022-06-01 12:10:19 +0200

    Simple main window with grid with databinding to see of the "row selection bug" can be easily reproduced but failing.

---
+++ src/WpfApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows;
+
+ [assembly: ThemeInfo(
+     ResourceDictionaryLocation.None,
+     ResourceDictionaryLocation.SourceAssembly)]
---
+++ src/WpfApp1/ViewModels/ViewModel1.cs
@@ -0,0 +1,8 @@
+﻿namespace WpfApp1.ViewModels
+{
+    using DevExpress.Mvvm;
+
+    public class ViewModel1 : ViewModelBase
+    {
+    }
+}
